6e086ef8120fb4a46081a5355cca3dbbcd075b55
Fathir <Fathironmy3@gmail.com>
2025-04-29 17:55:19 +0700

    penambahan konfigurasi vercel

--- api/index.js
+++ api/index.js
@@ -1,21 +1,25 @@
 const express = require('express')
+const pendaftarRouter = require('../src/routers/pendaftarRoute')
+
 const app = express()
 const port = 3000
-const pendaftarRouter = require('./routers/pendaftarRoute')
 const daftarRouter = require('./routers/daftarRoute')
 
 // Middleware untuk parsing JSON
 app.use(express.json())
 
-// Route untuk halaman utama
-app.get('/', (req, res) => {
-  res.send('Selamat datang di API pendaftaran!')
-})
-
-app.use('/', pendaftarRouter)
+// Routes
+app.use('/api', pendaftarRouter)
 app.use('/', daftarRouter)
 
+// Health check endpoint
+app.get('/', (req, res) => {
+  res.json({ message: 'API is running' })
+})
 
 app.listen(port, () => {
   console.log(`Server berjalan di http://localhost:${port}`)
 })
+
+// Export the Express API
+module.exports = app
