75cd6d07ba7bbbe3707d6437e27513b407cb05bf
Minba <fathironmy3@gmail.com>
2025-04-29 18:23:36 +0700

    fix bug api import module

--- api/index.js
+++ api/index.js
@@ -1,15 +1,12 @@
 const express = require('express')
+const app = express()
 const pendaftarRouter = require('../src/routers/pendaftarRoute')
 
-const app = express()
 const port = 3000
-const daftarRouter = require('./routers/daftarRoute')
-
-// Middleware untuk parsing JSON
-app.use(express.json())
+const daftarRouter = require('../src/routers/daftarRoute')
 
 // Routes
-app.use('/api', pendaftarRouter)
+app.use('/', pendaftarRouter)
 app.use('/', daftarRouter)
 
 // Health check endpoint
@@ -21,5 +18,4 @@ app.listen(port, () => {
   console.log(`Server berjalan di http://localhost:${port}`)
 })
 
-// Export the Express API
-module.exports = app
+
